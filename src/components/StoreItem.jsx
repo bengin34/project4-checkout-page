8df5ec30ddfa10a1501de9fabfc6c7616b32b333
bengin34 <becaglar3434@gmail.com>
2023-02-27 19:47:53 +0300

    some problems

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -1,23 +1,42 @@
-import React from 'react'
-import { Card } from 'react-bootstrap'
-import { formatCurrency } from '../utilities/formatCurrency'
+import React from "react";
+import { Card } from "react-bootstrap";
+import { formatCurrency } from "../utilities/formatCurrency";
+import { Button } from "react-bootstrap";
 
-
-
-const StoreItem = ({id,name,price,image,amount,dampingRate}) => {
-    const quantity = 0
+const StoreItem = ({ id, name, price, image, amount, dampingRate }) => {
+  const quantity = 0;
   return (
     <Card>
-      <Card.Img variant='top' src={image} height="300px"  style={{onjectFit:"cover", maxWidth:"300px", margin:"0 auto"} } className="align-items-center"/>
-    <Card.Body className='d-flex flex-column' >
-        <Card.Title className='d-flex justify-content-space-between align-items-baseline mb-4' >
-            <span className='fs-2'>{name}</span>
-            <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
+      <Card.Img
+        variant="top"
+        src={image}
+        height="300px"
+        style={{ onjectFit: "cover", maxWidth: "300px", margin: "0 auto" }}
+        className="align-items-center"
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-space-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
-        <div className='mt-auto'
-    </Card.Body>
+        <div className="mt-auto">
+          {quantity === 0 ? (
+            <Button className="w-100"> + Add To Cart</Button>
+          ) : (
+            <div
+              className="d-flex align-items-center flex-column"
+              style={{ gap: ".5rem" }}
+            >
+              <div
+                className="d-flex align-items-center justify-content-center"
+                style={{ gap: ".5rem" }}
+              ></div>
+            </div>
+          )}
+        </div>
+      </Card.Body>
     </Card>
-  )
-}
+  );
+};
 
-export default StoreItem
+export default StoreItem;
